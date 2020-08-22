ccf2e17edbd3916738b6ebc064469e65bb07f050
zwbetz-gh <zwbetz@gmail.com>
2020-08-22 15:34:03 -0500

    remove import and use page obj syntax for element

--- src/tests/template_api_playground_test.js
+++ src/tests/template_api_playground_test.js
@@ -1,5 +1,4 @@
 const hooks = require('../shared/hooks');
-const template_page = require('../page_objects/template_page');
 
 module.exports = {
   '@disabled': false,
@@ -17,55 +16,48 @@ module.exports = {
   'api playground': (browser) => {
     browser.page.template_page().navigate();
 
-    browser.assert.attributeContains(
-      template_page.elements.number1,
-      'class',
-      'form-control'
-    );
+    browser.page
+      .template_page()
+      .assert.attributeContains('@number1', 'class', 'form-control');
 
-    browser.assert.attributeEquals(
-      template_page.elements.number1,
-      'class',
-      'form-control form-control-lg'
-    );
+    browser.page
+      .template_page()
+      .assert.attributeEquals(
+        '@number1',
+        'class',
+        'form-control form-control-lg'
+      );
 
     browser.assert.containsText('h1', 'Sample');
 
-    browser.assert.cssClassPresent(template_page.elements.number1, [
-      'form-control',
-      'form-control-lg'
-    ]);
+    browser.page
+      .template_page()
+      .assert.cssClassPresent('@number1', ['form-control', 'form-control-lg']);
 
-    browser.assert.cssProperty(
-      template_page.elements.number1,
-      'display',
-      'block'
-    );
+    browser.page
+      .template_page()
+      .assert.cssProperty('@number1', 'display', 'block');
 
-    browser.assert.cssProperty(
-      template_page.elements.number1,
+    browser.page.template_page().assert.cssProperty(
+      '@number1',
       'color',
       'rgba(73, 80, 87, 1)' // #495057
     );
 
-    browser.assert.domPropertyContains(
-      template_page.elements.number1,
-      'tagName',
-      'INPUT'
-    );
+    browser.page
+      .template_page()
+      .assert.domPropertyContains('@number1', 'tagName', 'INPUT');
 
-    browser.assert.domPropertyEquals(
-      template_page.elements.operation,
-      'tagName',
-      'SELECT'
-    );
+    browser.page
+      .template_page()
+      .assert.domPropertyEquals('@operation', 'tagName', 'SELECT');
 
-    browser.assert.elementPresent(template_page.elements.number1);
+    browser.page.template_page().assert.elementPresent('@number1');
 
     browser.assert.title('Sample Calculator App');
 
     browser.assert.urlContains('http');
 
-    browser.assert.visible(template_page.elements.number1);
+    browser.page.template_page().assert.visible('@number1');
   }
 };
