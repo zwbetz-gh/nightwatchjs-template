4bb15a1532fc7487412fea39582c3e12b024fe1e
zwbetz-gh <zwbetz@gmail.com>
2020-09-26 21:13:08 -0500

    jsdoc for page helper

--- src/shared/page_helper.js
+++ src/shared/page_helper.js
@@ -1,4 +1,12 @@
-const click = (browser, selector, text, viaJs) => {
+/**
+ * Click an element.
+ *
+ * @param {object} browser - The browser object
+ * @param {string} selector - The CSS selector
+ * @param {string} [text] - The element text. If truthy, verify the element text
+ * @param {boolean} [viaJs] - If true, click the element via JS. If false, click the element via WebDriver
+ */
+const click = (browser, selector, text, viaJs = false) => {
   browser.waitForElementVisible(selector);
   browser.expect.element(selector).to.be.enabled;
 
@@ -13,6 +21,14 @@ const click = (browser, selector, text, viaJs) => {
   }
 };
 
+/**
+ * Set the value of an element.
+ *
+ * @param {object} browser - The browser object
+ * @param {string} selector - The CSS selector
+ * @param {string} value - The value to set
+ * @param {boolean} [verifyValue] - If true, verify the value that was set
+ */
 const setValue = (browser, selector, value, verifyValue = true) => {
   browser.waitForElementVisible(selector);
   browser.expect.element(selector).to.be.enabled;
@@ -23,21 +39,47 @@ const setValue = (browser, selector, value, verifyValue = true) => {
   }
 };
 
+/**
+ * Verify the value of an element.
+ *
+ * @param {object} browser - The browser object
+ * @param {string} selector - The CSS selector
+ * @param {string} value - The value to verify
+ */
 const verifyValue = (browser, selector, value) => {
   browser.waitForElementVisible(selector);
   browser.expect.element(selector).value.to.equal(value);
 };
 
+/**
+ * Verify the text of an element.
+ *
+ * @param {object} browser - The browser object
+ * @param {string} selector - The CSS selector
+ * @param {string} text - The text to verify
+ */
 const verifyText = (browser, selector, text) => {
   browser.waitForElementVisible(selector);
   browser.expect.element(selector).text.to.equal(text);
 };
 
+/**
+ * Verify that an element is enabled.
+ *
+ * @param {object} browser - The browser object
+ * @param {string} selector - The CSS selector
+ */
 const verifyEnabled = (browser, selector) => {
   browser.waitForElementVisible(selector);
   browser.expect.element(selector).to.be.enabled;
 };
 
+/**
+ * Verify that an element is not enabled.
+ *
+ * @param {object} browser - The browser object
+ * @param {string} selector - The CSS selector
+ */
 const verifyNotEnabled = (browser, selector) => {
   browser.waitForElementVisible(selector);
   browser.expect.element(selector).to.not.be.enabled;
