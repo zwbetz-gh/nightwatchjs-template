4133abea45f4c393fabf4be0deaa0bcfea493cf5
zwbetz-gh <zwbetz@gmail.com>
2020-09-29 00:57:15 -0500

    github issue https://github.com/nightwatchjs/nightwatch/issues/2503

---
+++ src/tests/various/github_issue_for_expect_element_value_in_geckodriver.js
@@ -0,0 +1,43 @@
+module.exports = {
+  '@disabled': false,
+
+  '@tags': ['https://github.com/nightwatchjs/nightwatch/issues/2503'],
+
+  'github issue for expect element value in geckodriver': (browser) => {
+    const scriptBody = function () {
+      const el = document.createElement('input');
+      el.type = 'text';
+      el.id = 'some_input';
+      document.body.appendChild(el);
+    };
+
+    const scriptArgs = [];
+
+    const callback = function () {
+      console.log('execute is complete');
+    };
+
+    browser.execute(scriptBody, scriptArgs, callback);
+
+    browser.source((response) => {
+      console.log('source:', response.value);
+    });
+
+    browser.setValue('#some_input', 'some value');
+
+    browser.pause(1000);
+
+    browser.getValue('#some_input', (response) => {
+      console.log('getValue:', response.value);
+    });
+
+    browser.assert.value('#some_input', 'some value');
+
+    browser.expect
+      .element('#some_input')
+      .value.to.equal('some value')
+      .before(100);
+
+    browser.end();
+  }
+};
